22bc679f7740c0a6cc7948e9aa410ba9c25d8fb9
carlallanvela <carlallanvela@gmail.com>
2019-05-29 17:31:50 +0800

    [CVELA] JPA implementation.
    JPA implementation

--- src/main/java/com/cvela/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/com/cvela/rest/webservices/restfulwebservices/todo/Todo.java
@@ -2,13 +2,21 @@ package com.cvela.rest.webservices.restfulwebservices.todo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Todo Bean.
  * @author cvela
  *
  */
+@Entity
 public class Todo {
-	private long id;
+	@Id
+	@GeneratedValue
+	private Long id; 
+	
 	private String username;
 	private String description;
 	private Date targetDate;
@@ -25,11 +33,11 @@ public class Todo {
 		this.isDone = isDone;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo(id, username, description, target_date, is_done)
+values (10001, 'cvela', 'Learn Angular', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10002, 'carlos', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10003, 'cvela', 'Learn JWT', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10004, 'carlos', 'Learn SpringBoot', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10005, 'cvela', 'Learn React', sysdate(), false);
